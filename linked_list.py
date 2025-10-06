9b85fd4e5076b64570d1a0df0e64e90b584e0951
bharath6969 <bharathreddy.v.v.r@gmail.com>
2025-10-05 18:31:45 -0700

    This is to implement min stack using one list

---
+++ linked_list.py
@@ -0,0 +1,43 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.hass = [None] * 1000
+        self.plength = 1000
+        self.selength = 1000
+
+    def has1(self, key):
+        return key % self.plength
+
+    def has2(self, key):
+        return key / self.selength
+
+    def add(self, key: int) -> None:
+        val1 = self.has1(key)
+        val2 = self.has2(key)
+
+        if self.hass[val1] is None:
+            self.hass[val1] = [False] * self.selength
+        self.hass[val1][val2] = True
+
+    def remove(self, key: int) -> None:
+        val1 = self.has1(key)
+        val2 = self.has2(key)
+
+        if self.hass[val1] is None:
+            return
+        else:
+            self.hass[val1][val2] = False
+
+    def contains(self, key: int) -> bool:
+        val1 = self.has1(key)
+        val2 = self.has2(key)
+
+        if self.hass[val1] is None:
+            return False
+        return self.hass[val1][val2]
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
